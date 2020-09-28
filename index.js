473684d8e29fe847abe8f6c9536f6689c43cc149
chrunlee <chrunlee@foxmail.com>
2020-09-28 11:06:15 +0800

    覆盖config

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const {COMPARISON_LIST} = require('think-model-abstract');
 const Model = require('./lib/model.js');
 const mysql = require('think-model-mysql');
 
+let defaultConfig = {
+    prefix: '',
+    encoding: 'utf8mb4',
+    dateStrings: true,
+    logSql: false
+};
 function model(config) {
+  config = helper.extend(defaultConfig,config);
   const Cls = Model;
   config.handle = mysql;
   const modelName = 'mysql';
